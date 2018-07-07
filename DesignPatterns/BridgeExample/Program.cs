7d2fca63bc6a1a8ad0da085b9fa402c148db0c12
nastyagurskaya <just.gurskaya@gmail.com>
2018-07-07 15:54:04 +0300

    NOTE-1678 Implemented Bridge and Flyweight pattern examples

--- DesignPatterns/BridgeExample/Program.cs
+++ DesignPatterns/BridgeExample/Program.cs
@@ -10,8 +10,92 @@ namespace BridgeExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Artist painter = new Painter("Raffaello Santi", new Star());
+            painter.DoWork();
+            painter.Shape = new Square();
+            painter.DoWork();
+            Console.Read();
+        }
+    }
+    interface IShape
+    {
+        void Draw();
+        void Rotate();
+    }
+
+    class Star : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Star was drawn");
+        }
+
+        public void Rotate()
+        {
+            Console.WriteLine("Star rotated");
+        }
+    }
+
+    class Square : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Square was drawn");
+        }
+
+        public void Rotate()
+        {
+            Console.WriteLine("Square rotated");
+        }
+    }
+    abstract class Artist
+    {
+        protected IShape shape;
+        public IShape Shape
+        {
+            set { shape = value; }
+        }
+        public Artist(IShape sh)
+        {
+            shape = sh;
+        }
+        public abstract void DoWork();
+        public void DrawShape()
+        {
+            shape.Draw();
+        }
+        public void RotateShape()
+        {
+            shape.Rotate();
+        }
+    }
+
+    class Painter : Artist
+    {
+        private String name;
+        public Painter(String n, IShape sh) : base(sh)
+        {
+            name = n;
+        }
+        public override void DoWork()
+        {
+            DrawShape();
+
+            Console.WriteLine(" by Painter:" +name);
+        }
+    }
+    class Designer : Artist
+    {
+        private String name;
+        public Designer(String n, IShape sh) : base(sh)
+        {
+            name = n;
+        }
+        public override void DoWork()
+        {
+            RotateShape();
+
+            Console.WriteLine(" by Designer:" + name);
         }
     }
 }
